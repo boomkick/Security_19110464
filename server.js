f81876353a5f4ae92cf6138353a8de8fdb7f4db5
boomkick <tranmanhthang107@gmail.com>
2022-11-30 01:54:02 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,78 @@
+const fs = require('fs');
+const path = require('path');
+const https = require('https');
+const express = require('express');
+const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+
+require('dotenv').config();
+
+const PORT = 3000;
+
+const config = {
+	CLIENT_ID: process.env.CLIENT_ID,
+	CLIENT_SECRET: process.env.CLIENT_SECRET,
+}
+
+const AUTH_OPTIONS = {
+	callbackURL: '/auth/google/callback',
+	clientID: config.CLIENT_ID,
+	clientSecret: config.CLIENT_SECRET
+}
+
+function verifyCallback(accessToken, refreshToken, profile, done){
+	console.log('Google profile', profile);
+	done(null, profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
+
+const app = express();
+
+app.use(helmet());
+app.use(passport.initialize())
+
+function checkLoggedIn(req, res, next) {
+	const isLoggedIn = true;
+	if(!isLoggedIn) {
+		return res.status(401).json({
+			error: ('You must log in!'),
+		});
+	}
+	next();
+}
+
+app.get('/auth/google', passport.authenticate('google', {
+	scope: ['email'],
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google', {
+	failureRedirect: '/failure',
+	successRedirect: '/',
+	session: false,
+}), (req, res) => {
+	console.log('Google called us back!');
+});
+
+app.get('/auth');
+
+app.get('/secret', checkLoggedIn, (req, res) => {
+	return res.send('Your personal secret is 42!');
+});
+
+app.get('/failure', (req, res) => {
+	return res.send('fail to connect GG');
+})
+
+app.get('/', (req, res) => {
+
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+}, app).listen(PORT, () => {
+	console.log(`Listening on port ${PORT}...`);
+});
